c72caab28d1c93912f3ee2b61d03f5e6bc1df68b
jse13 <jse13@my.fsu.edu>
2019-02-16 15:17:55 -0800

    macro for iterm and bspc fix

--- crumchy/keymap.c
+++ crumchy/keymap.c
@@ -40,6 +40,7 @@ enum custom_keycodes {
   RAISE,
   FCN,
   NAV,
+  ITERM_MACRO,
 };
 
 // Fillers to make layering more clear
@@ -59,23 +60,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl | Alt  |      | Bksp | GUI  |Lower |Raise |Space |Enter |      |      |      |
  * `-----------------------------------------------------------------------------------' */
 [_QWERTY] = LAYOUT_ortho_4x12( \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  KC_LCTL,  KC_LALT, MO(_LOWER),  KC_BSPC, KC_LGUI, MO(_LOWER),  MO(_RAISE), KC_SPC, KC_ENT, _______, _______, _______\
+  KC_LCTL,  KC_LALT, MO(_LOWER),  KC_BSPC, KC_LGUI, MO(_LOWER),  MO(_RAISE), KC_SPC, KC_ENT, _______, _______, ITERM_MACRO\
 ),
 
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |     |    \  |  |   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
 [_LOWER] = LAYOUT_ortho_4x12( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
   KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRAVE, \
@@ -171,6 +161,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case ITERM_MACRO:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LALT(SS_LGUI(SS_TAP(X_SPACE))));
+      }
   }
   return true;
 }
