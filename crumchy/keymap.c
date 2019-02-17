b90c8187a40e4827706d208c936d7cdc57d2f9f1
jse13 <jse13@my.fsu.edu>
2019-02-16 19:17:03 -0800

    remove enter binding and add bspace

--- crumchy/keymap.c
+++ crumchy/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_4x12( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, \
   KC_LCTL,  KC_LALT, MO(_LOWER),  KC_BSPC, KC_LGUI, MO(_LOWER),  MO(_RAISE), KC_SPC, KC_ENT, _______, _______, ITERM_MACRO\
 ),
 
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
-  _______, TD( TD_LTGT ), TD( TD_PARENS ), TD( TD_SQUARES ), TD( TD_CURLIES), _______, _______, _______, _______, _______, _______, _______,\
+  _______, TD( TD_LTGT ), TD( TD_PARENS ), TD( TD_SQUARES ), TD( TD_CURLIES), _______, _______, _______, _______, _______, _______, KC_BSLASH,\
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
   _______, _______, _______, _______, _______, MO( _NAV ), _______, _______, _______, _______, _______, _______\
 ),
