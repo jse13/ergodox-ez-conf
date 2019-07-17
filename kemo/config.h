02c4ddf4a64a9e01524fceeeaac5ebe430fadbc1
jse13 <me@julianengel.com>
2019-07-16 17:59:39 -0700

    rewrite startup sound so it actually sounds correct

--- kemo/config.h
+++ kemo/config.h
@@ -2,13 +2,7 @@
 
 #undef MEGALOVANIA
 #define MEGALOVANIA \
-  Q__NOTE(_D4), Q__NOTE(_D4), H__NOTE(_D5), HD_NOTE(_A4), H__NOTE(_AF4), H__NOTE(_G4), H__NOTE(_F4), \
-  Q__NOTE(_D4), Q__NOTE(_F4), Q__NOTE(_G4), Q__NOTE(_C4), Q__NOTE(_C4), H__NOTE(_D5), HD_NOTE(_A4), \
-  H__NOTE(_AF4), H__NOTE(_G4), H__NOTE(_F4), Q__NOTE(_D4), Q__NOTE(_F4), Q__NOTE(_G4), \
-  Q__NOTE(_B3), Q__NOTE(_B3), H__NOTE(_D5), HD_NOTE(_A4), H__NOTE(_AF4), H__NOTE(_G4), H__NOTE(_F4), \
-  Q__NOTE(_D4), Q__NOTE(_F4), Q__NOTE(_G4), Q__NOTE(_BF3), Q__NOTE(_BF3), H__NOTE(_D5), \
-  HD_NOTE(_A4), H__NOTE(_AF4), H__NOTE(_G4), H__NOTE(_F4), Q__NOTE(_D4), Q__NOTE(_F4), \
-  Q__NOTE(_G4),
+  E__NOTE(_D5), E__NOTE(_REST), E__NOTE(_D5), E__NOTE(_REST), Q__NOTE(_D6),
 
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(MEGALOVANIA)
